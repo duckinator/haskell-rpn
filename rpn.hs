e76885fb5714c8d2dcdd54fa58b5e776e9ace0e1
Marie Markwell <me@marie.so>
2014-09-26 02:57:41 -0400

    Improvements based on @CodeBlock's feedback.

--- rpn.hs
+++ rpn.hs
@@ -1,23 +1,25 @@
+import Text.Read
+
 data Thing = Operator String | Operand Float
+
+instance Show Thing where
+  show (Operator x) = x
+  show (Operand x)  = show x
 
 str2float :: String -> Float
 str2float str = read str :: Float
 
-showThing :: Thing -> String
-showThing (Operator x) = x
-showThing (Operand x)  = show x
-
 operator :: String -> Thing
-operator x = Operator x
+operator = Operator
 
 operand :: String -> Thing
 operand x = Operand $ str2float x
 
 solveRPN :: String -> String
-solveRPN = showThing . head . solveRPN' . thingify . words
+solveRPN = show . head . solveRPN' . thingify . words
 
 thingify :: [String] -> [Thing]
-thingify xs = map thingify' xs
+thingify = map thingify'
 
 thingify' :: String -> Thing
 thingify' "+" = operator "+"
@@ -32,7 +34,7 @@ solveRPN' xs = xs
 
 solveRPNStatement :: Thing -> Thing -> Thing -> Thing
 solveRPNStatement (Operand l) (Operand r) (Operator op) = Operand $ solveRPNStatement' l r op
-solveRPNStatement l r op = error ("Don't know what to do  with " ++ (showThing l) ++ " " ++ (showThing r) ++ " " ++ (showThing op))
+solveRPNStatement l r op = error ("Don't know what to do  with " ++ (show l) ++ " " ++ (show r) ++ " " ++ (show op))
 
 solveRPNStatement' :: Float -> Float -> String -> Float
 solveRPNStatement' l r "+" = l + r
